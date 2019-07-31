f26f3400f03b76bf49c50b444e1e9d5fddb58104
Christian Toscano <chrismarcadopago@gmail.com>
2019-07-31 15:05:00 -0300

    conexion a mongo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const model = require('../model/task')();
+
 router.get('/', (req, res) => {
-    res.render('index',{
-       title: 'CRUD' 
-    });
+    model.find({}, (err, tasks) => {
+        if (err) throw err;
+        res.render('index',{
+            title: 'CRUD',
+            task: tasks
+         });
+    })
 });
 
 module.exports = router;
