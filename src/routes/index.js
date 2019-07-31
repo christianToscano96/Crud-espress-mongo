d858f0b4131d0cf2e3ec552a3d670da670d2346f
Christian Toscano <chrismarcadopago@gmail.com>
2019-07-31 16:22:05 -0300

    metodos delete y update

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,7 +21,27 @@ router.post('/add', (req, res) => {
     model.create(body, (err, task) => {
         if (err) throw err;
         res.redirect('/');
-    })    
-})
+    });    
+});
+
+//UPDATE
+router.get('/turn/:id', (req, res) => {
+    let id = req.params.id;
+    model.findById(id, (err, task) => {
+        if (err) throw err;
+        task.status = !task.status;
+        task.save()
+          .then(() => res.redirect('/'))
+    });
+});
+
+//DELETE
+router.get('/delete/:id', (req, res) => {
+    let id = req.params.id;
+    model.remove({_id: id}, (err, task) => {
+        if (err) throw err;
+        res.redirect('/')
+    });
+});
 
 module.exports = router;
